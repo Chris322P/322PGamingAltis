15f996a6db852496d0c781320e8f1f7c5c563864
Chris322P <Chris322P@googlemail.com>
2014-06-11 02:31:55 +0200

    New Interface init test

--- Altis_Life.Altis/dialog/player_inv - Kopie.hpp
+++ Altis_Life.Altis/dialog/player_inv - Kopie.hpp
@@ -8,35 +8,33 @@ class playerSettings {
 	
 	class controlsBackground {
 	
-		class life_RscTitleBackground:life_RscText { //redone
-			colorBackground[] = {0.85,0.25,0.25,1};				
-			text = "Charakter Window"; //--- ToDo: Localize;
+	class life_RscTitleBackground:life_RscText {
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
 			idc = -1;
-			x = 4.5 * GUI_GRID_W + GUI_GRID_X;
-			y = 3.5 * GUI_GRID_H + GUI_GRID_Y;
-			w = 31.5 * GUI_GRID_W;
-			h = 1 * GUI_GRID_H;
-			sizeEx = 1 * GUI_GRID_H;
+			x = 0.1;
+			y = 0.2;
+			w = 0.8;
+			h = (1 / 25);
 		};
 		
-		class MainBackground:life_RscText { //redone
+		class MainBackground:life_RscText {
 			colorBackground[] = {0, 0, 0, 0.7};
 			idc = -1;
-			x = 4.5 * GUI_GRID_W + GUI_GRID_X;
-			y = 4.5 * GUI_GRID_H + GUI_GRID_Y;
-			w = 31.5 * GUI_GRID_W;
-			h = 13 * GUI_GRID_H;
+			x = 0.1;
+			y = 0.2 + (11 / 250);
+			w = 0.8;
+			h = 0.6 - (22 / 250);
 		};
 		
-		// class Title : life_RscTitle {
-			// colorBackground[] = {0, 0, 0, 0};
-			// idc = -1;
-			// text = "$STR_PM_Title";
-			// x = 0.1;
-			// y = 0.2;
-			// w = 0.8;
-			// h = (1 / 25);
-		// };
+		class Title : life_RscTitle {
+			colorBackground[] = {0, 0, 0, 0};
+			idc = -1;
+			text = "$STR_PM_Title";
+			x = 0.1;
+			y = 0.2;
+			w = 0.8;
+			h = (1 / 25);
+		};
 		
 		class moneyStatusInfo : Life_RscStructuredText
 		{
@@ -48,11 +46,11 @@ class playerSettings {
 			w = 0.3; h = 0.6;
 		};
 		
-		//class PlayersName : Title {
-		//	idc = carry_weight;
-		//	style = 1;
-		//	text = "";
-		//};
+		class PlayersName : Title {
+			idc = carry_weight;
+			style = 1;
+			text = "";
+		};
 	};
 	
 	class controls {
